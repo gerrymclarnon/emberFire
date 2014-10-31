9e564c5d0499fff9786d53b7257af3d41da3b50f
Gerry McLarnon <gerry.mclarnon@beatsystems.com>
2014-10-31 13:20:57 +0000

    Removed Login

--- examples/todomvc/js/app.js
+++ examples/todomvc/js/app.js
@@ -5,21 +5,3 @@ var commentsRef = new Firebase('https://todomvc-ember.firebaseio-demo.com/');
 Todos.ApplicationAdapter = DS.FirebaseAdapter.extend({
 	firebase: new Firebase("https://todomvc-ember.firebaseio-demo.com/")
 });
-
-//Create an Firebase Simple Login client so we can do Facebook auth
-var auth = new FirebaseSimpleLogin(commentsRef, function(error, user) {
-    if (error) {
-        // an error occurred while attempting login
-        console.log(error);
-    } else if (user) {
-        myUserID = user.id;
-        $("#loginDiv").text(user.first_name + " " + user.last_name);
-    } else {
-        // User logged out
-    }
-});
-
-//Handle Login
-function onLoginButtonClicked() {
-    auth.login("password", { email: "person@firebase.com", password: "blah" });
-}
